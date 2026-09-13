e84e001faf8bb425f42fb1a51e010e2a835e89c9
Your Name <your@email.com>
2026-09-13 17:01:19 +0545

    Enable CORS

--- api.py
+++ api.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 import joblib
 import pandas as pd
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 model = joblib.load("grapevine_rf_visible_tuned.joblib")
 
